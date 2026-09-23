38e2d20a64dfb8c4d43631f5584aebfed72b81b7
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-23 07:12:43 -0300

    assets: link final Region 4 island 04 pair

--- web/js/content/game-content.js
+++ web/js/content/game-content.js
@@ -912,8 +912,8 @@
               locked: "./assets/regions/region-4/coroa-do-inverno-locked.webp?v=20260923-terras-gelidas-ilha5-v1"
           }),
                     4: Object.freeze({
-              unlocked: "./assets/regions/region-4/fortaleza-da-nevasca-unlocked.webp?v=20260923-terras-gelidas-ilha4-v1",
-              locked: "./assets/regions/region-4/fortaleza-da-nevasca-locked.webp?v=20260923-terras-gelidas-ilha4-v1"
+              unlocked: "./assets/regions/region-4/island-04-unlocked.webp?v=20260923-terras-gelidas-ilha4-final",
+              locked: "./assets/regions/region-4/island-04-locked.webp?v=20260923-terras-gelidas-ilha4-final"
           })
                 })
             }),
