365b91036360d24afb49fb0b7fe776a009acdd27
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-25 15:42:19 -0300

    chore(web): invalidate cache after FX removal

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260925-up-inventory-v1";
+const CACHE_VERSION = "tq2-dev-assets-20260925-no-fx-v1";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
