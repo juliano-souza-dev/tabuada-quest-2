2368b997aa588a275f37e6a3165055255ccce12a
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-25 16:44:02 -0300

    feat(web): invalidate cache for dual Home avatars

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260925-home-scenery20-v1";
+const CACHE_VERSION = "tq2-dev-assets-20260925-home-dual-avatar-v1";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
