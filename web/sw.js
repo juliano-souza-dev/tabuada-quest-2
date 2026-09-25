83deb15b60142e6ac17adaaf48141399e9d7bb07
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-25 14:31:39 -0300

    fix(web): invalidate cache after composition registration fix

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260925-asset-classify-v3";
+const CACHE_VERSION = "tq2-dev-assets-20260925-asset-classify-v4";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
