454762cef1c7c382d2f9cdca2c918febf0096a90
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-25 13:20:48 -0300

    refactor(web): invalidate PWA asset cache for full reset

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260924-restore-01";
+const CACHE_VERSION = "tq2-dev-assets-20260925-full-layout-reset-v1";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
