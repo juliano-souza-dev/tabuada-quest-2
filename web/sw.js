1c077fa170001ca5dfe10391f34c77c872880611
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-25 17:59:58 -0300

    feat(web): invalidate cache for CENA element behaviors

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260925-ux-hide-assets-v1";
+const CACHE_VERSION = "tq2-dev-assets-20260925-cena-element-behavior-v1";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
