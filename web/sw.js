b7308bdc6fd3b9ec5b453a964ce433fadd873452
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-25 23:15:08 -0300

    chore(dev): invalidate cache for UP context fix

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260925-upload-transaction-v1";
+const CACHE_VERSION = "tq2-dev-assets-20260925-upload-context-v1";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
