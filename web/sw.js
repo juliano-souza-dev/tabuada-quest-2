b3aea5c10d98c3741a6b16eb87c61cd02a96da4d
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-26 00:31:32 -0300

    chore(dev): invalidate cache for global geometry editor

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260925-site-assets-editable-v1";
+const CACHE_VERSION = "tq2-dev-assets-20260925-global-geometry-v1";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
