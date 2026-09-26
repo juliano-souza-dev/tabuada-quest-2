67684bb5ec0853be7f7cfbf83dccb8c170deccba
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-26 10:22:11 -0300

    chore(dev): invalidate remove-art cache

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260926-home-0945-exact-v1";
+const CACHE_VERSION = "tq2-dev-assets-20260926-remove-art-tombstone-v1";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
