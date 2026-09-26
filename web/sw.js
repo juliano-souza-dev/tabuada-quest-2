0bdbc39b969b4683e98be17eacb1bf48d1481151
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-26 13:15:10 -0300

    chore(dev): invalidate UX visual bounds cache

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260926-ocean-point-area-v3";
+const CACHE_VERSION = "tq2-dev-assets-20260926-ux-visual-bounds-v1";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
