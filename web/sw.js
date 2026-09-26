f22a6051bdd7ba8412293ade7d05b03b50b927ec
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-26 07:50:42 -0300

    chore(dev): invalidate cache for engine v4

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260926-home-v3-dynamic-ship";
+const CACHE_VERSION = "tq2-dev-assets-20260926-dev-engine-v4";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
