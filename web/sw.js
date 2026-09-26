e1589f66db7d92d1971847d3b0d19b933ad83d29
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-26 00:40:53 -0300

    chore(home): invalidate cache for composed Home

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260925-global-geometry-v1";
+const CACHE_VERSION = "tq2-dev-assets-20260926-home-default-composition-v1";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
