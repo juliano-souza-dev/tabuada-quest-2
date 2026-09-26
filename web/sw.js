f3d4ce014b0849ac46f61792ca6bcdc396eb0047
juliano-souza-dev <souzza.s1990@gmail.com>
2026-09-26 11:39:16 -0300

    chore(dev): invalidate ZIP importer cache

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tq2-dev-assets-20260926-up-live-sync-v1";
+const CACHE_VERSION = "tq2-dev-assets-20260926-zip-package-import-v1";
 const CACHE_PREFIX = "tq2-dev-assets-";
 
 self.addEventListener("install", (event) => {
